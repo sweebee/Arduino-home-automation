40fc5fd26f3599cf7f4d45ec28c874c584e2fb4b
Wiebe Nieuwenhuis <Wiebe@MacBook-Pro-van-Wiebe.local>
2015-05-05 23:20:30 +0200

    added test sensor

---
+++ 2.4Ghz (MySensors)/Arduino/testSensor/testSensor.ino
@@ -0,0 +1,28 @@
+#include <MySensor.h>
+#include <SPI.h>
+
+#define SENSOR_INFO "Test sensor"
+#define NODE_ID 10
+#define CHILD_ID 1
+#define OPEN 1
+#define CLOSE 0
+ 
+MySensor gw;
+MyMessage msg(CHILD_ID, V_TRIPPED);
+ 
+void setup() 
+{ 
+  gw.begin(NULL, NODE_ID, false);
+  gw.sendSketchInfo(SENSOR_INFO, "1.0");
+  gw.present(CHILD_ID, S_DOOR); 
+}
+ 
+void loop()
+{
+     gw.send(msg.set(OPEN)); 
+  
+     delay(500); // Wait 10 seconds
+     gw.send(msg.set(CLOSE));
+
+     delay(500); // Wait 10 seconds
+}
